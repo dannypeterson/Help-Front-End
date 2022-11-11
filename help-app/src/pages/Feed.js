b167318b6d4543a5eb4d733cbcc12a7d47ae3aad
Danny <dannypeterson28@gmail.com>
2022-11-11 15:57:05 -0500

    mapped out all reviews

--- help-app/src/pages/Feed.js
+++ help-app/src/pages/Feed.js
@@ -1,9 +1,36 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 import Nav from '../components/Nav'
+import { BASE_URL } from '../services/api'
 
 const Feed = () => {
+  const [reviews, setReviews] = useState()
+
+  const getReviews = async () => {
+    const res = await axios.get(`${BASE_URL}/reviews`)
+    console.log(res)
+    setReviews(res.data)
+  }
+
+  useEffect(() => {
+    getReviews()
+  }, [])
+
   return (
     <div className="main">
       <Nav />
+      <div className="reviews">
+        {reviews?.map((review) => (
+          <div key={review.id}>
+            <h3>{review.userId}</h3>
+            <h2>{review.dish}</h2>
+            <h3>from {review.restaurantId}</h3>
+            <img src={review.img} alt=""></img>
+            <p>{review.description}</p>
+            <h3>Rating: {review.rating}/5</h3>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
