5f91d635512b9a79c5e8970fd064b6118efef27f
Maciej Parol <maciejparol333@gmail.com>
2022-11-16 16:22:27 -0500

    landing page down

--- help-app/src/components/Login.js
+++ help-app/src/components/Login.js
@@ -1,69 +1,69 @@
-import { SignInUser } from '../services/Auth'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+// import { SignInUser } from '../services/Auth'
+// import { useState } from 'react'
+// import { useNavigate } from 'react-router-dom'
 
-const Login = ({ setUser }) => {
-  const navigate = useNavigate()
-  const initialState = {
-    username: '',
-    email: '',
-    password: ''
-  }
-  const [signInState, setSignInState] = useState(initialState)
+// const Login = ({ setUser }) => {
+//   const navigate = useNavigate()
+//   const initialState = {
+//     username: '',
+//     email: '',
+//     password: ''
+//   }
+//   const [signInState, setSignInState] = useState(initialState)
 
-  const handleChange = (e) => {
-    setSignInState({ ...signInState, [e.target.id]: e.target.value })
-  }
+//   const handleChange = (e) => {
+//     setSignInState({ ...signInState, [e.target.id]: e.target.value })
+//   }
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    await SignInUser({
-      username: signInState.username,
-      email: signInState.email,
-      password: signInState.password
-    })
-    const payload = await SignInUser(signInState)
-    setUser(payload)
+//   const handleSubmit = async (e) => {
+//     e.preventDefault()
+//     await SignInUser({
+//       username: signInState.username,
+//       email: signInState.email,
+//       password: signInState.password
+//     })
+//     const payload = await SignInUser(signInState)
+//     setUser(payload)
 
-    setSignInState(initialState)
-    navigate('/feed')
-  }
+//     setSignInState(initialState)
+//     navigate('/feed')
+//   }
 
-  return (
-    <form className="signinform" onSubmit={handleSubmit}>
-      <label className="login" htmlFor="username">
-        Username:
-      </label>
-      <input
-        id="username"
-        type="text"
-        placeholder="enter your username"
-        onChange={handleChange}
-        value={signInState.username}
-      />
-      <br />
-      <label htmlFor="username">Email:</label>
-      <input
-        id="email"
-        type="text"
-        placeholder="enter your email"
-        onChange={handleChange}
-        value={signInState.email}
-      />
-      <br />
-      <label htmlFor="username">Password:</label>
-      <input
-        id="password"
-        type="text"
-        placeholder="create your password"
-        onChange={handleChange}
-        value={signInState.password}
-      />
-      <br />
+//   return (
+//     <form className="signinform" onSubmit={handleSubmit}>
+//       <label className="login" htmlFor="username">
+//         Username:
+//       </label>
+//       <input
+//         id="username"
+//         type="text"
+//         placeholder="enter your username"
+//         onChange={handleChange}
+//         value={signInState.username}
+//       />
+//       <br />
+//       <label htmlFor="username">Email:</label>
+//       <input
+//         id="email"
+//         type="text"
+//         placeholder="enter your email"
+//         onChange={handleChange}
+//         value={signInState.email}
+//       />
+//       <br />
+//       <label htmlFor="username">Password:</label>
+//       <input
+//         id="password"
+//         type="text"
+//         placeholder="create your password"
+//         onChange={handleChange}
+//         value={signInState.password}
+//       />
+//       <br />
 
-      <button type="submit">Sign in!</button>
-    </form>
-  )
-}
+//       <button type="submit">Sign in!</button>
+//     </form>
+//   )
+// }
 
-export default Login
+// export default Login
